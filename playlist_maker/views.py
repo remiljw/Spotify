584e2c56be3859aa4d3b0854e7c1750a6f30df90
Michael Ajanaku <remiljw@gmail.com>
2020-06-06 02:07:27 +0100

    edited views

--- playlist_maker/views.py
+++ playlist_maker/views.py
@@ -10,7 +10,7 @@ SPOTIPY_REDIRECT_URI = "http://localhost:8000/"
 scope = "playlist-modify-public"
 username = "ttbriyo504kilcgb12zr3tf6g"
 token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
-tp = spotipy.Spotify(auth=token)
+sp = spotipy.Spotify(auth=token)
 
 def index(request):
     return render(request, 'playlist_maker/index.html')
@@ -19,13 +19,14 @@ def artist_not_found(request):
     return render(request, 'playlist_maker/artist_not_found.html')
 
 
-def show_playlist(self,request):
-    desired_artist = self.request.GET("artist_to_search")
+def show_playlist(request):
+    if request.method == 'GET':
+        desired_artist = request.GET.get("artist_to_search")
     if not desired_artist:
-        return redirect('index')
+        return redirect('playlist_maker:index')
     desired_artist_results = sp.search(q="artist:" + desired_artist, type="artist")
     if len(desired_artist_results["artists"]["items"]) == 0:
-        return redirect('artist_not_found')
+        return redirect('playlist_maker:artist_not_found')
 
     desired_artist_entry = desired_artist_results["artists"]["items"][0]
     artist_name = desired_artist_entry["name"]
@@ -34,7 +35,7 @@ def show_playlist(self,request):
 
     playlist_name = "Inspired by " + artist_name
     sp.trace = False
-    playlist_name = sp.user_playlist_create(username, playlist_name)
+    playlist = sp.user_playlist_create(username, playlist_name)
     playlist_id = playlist["id"]
 
     list_of_tracks = []
@@ -55,4 +56,4 @@ def show_playlist(self,request):
         'frame' :playlist_iframe_href 
     }
 
-    return (request, 'playlist_maker/playlist.html', context)
+    return render(request, 'playlist_maker/playlist.html', context)
